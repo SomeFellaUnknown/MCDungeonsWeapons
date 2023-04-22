7463e1b6923ac7c5d3000162e4ac52865f8c4b5d
SomeFellaUnknown <vikeisavesume@gmail.com>
2023-04-22 20:21:54 +0200

    Add isAcceptableItem field to Guarding Strike
    it is the only enchant that is missing this field and it seems to be causing issues

--- src/main/java/chronosacaria/mcdw/enchants/enchantments/GuardingStrikeEnchantment.java
+++ src/main/java/chronosacaria/mcdw/enchants/enchantments/GuardingStrikeEnchantment.java
@@ -1,18 +1,21 @@
 package chronosacaria.mcdw.enchants.enchantments;
 
 import chronosacaria.mcdw.Mcdw;
+import chronosacaria.mcdw.bases.McdwCustomWeaponBase;
 import chronosacaria.mcdw.enums.EnchantmentsID;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentLevelEntry;
 import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.AxeItem;
 import net.minecraft.item.EnchantedBookItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 
 public class GuardingStrikeEnchantment extends Enchantment {
-
     public GuardingStrikeEnchantment(Rarity rarity, EnchantmentTarget enchantmentTarget, EquipmentSlot[] equipmentSlots) {
         super(rarity, enchantmentTarget, equipmentSlots);
         if (Mcdw.CONFIG.mcdwEnchantmentsConfig.ENABLE_ENCHANTMENTS.get(EnchantmentsID.GUARDING_STRIKE)) {
@@ -40,6 +43,11 @@ public class GuardingStrikeEnchantment extends Enchantment {
     public boolean isAvailableForEnchantedBookOffer() {
         return Mcdw.CONFIG.mcdwEnchantmentsConfig.ENABLE_ENCHANTMENTS.get(EnchantmentsID.GUARDING_STRIKE)
                 && Mcdw.CONFIG.mcdwEnchantmentsConfig.ENABLE_VILLAGER_TRADING.get(EnchantmentsID.GUARDING_STRIKE);
+    }
+    
+    @Override
+    public boolean isAcceptableItem(ItemStack stack) {
+        return stack.getItem() instanceof SwordItem || stack.getItem() instanceof AxeItem || stack.getItem() instanceof McdwCustomWeaponBase;
     }
 
     @Override
